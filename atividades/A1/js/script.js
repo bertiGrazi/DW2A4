48d8cea9eb8d993cc3c281af7af65eac16a21b13
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-10-02 23:35:49 -0300

    finalizando a aula02

---
+++ atividades/A1/js/script.js
@@ -0,0 +1,16 @@
+Modal = {
+    open(){
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document.querySelector('.modal-overlay')
+        .classList
+        .add('active')
+    },
+    close() {
+        // Fechar modal
+        // Remover a class active ao modal
+        document.querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+    }
+}
