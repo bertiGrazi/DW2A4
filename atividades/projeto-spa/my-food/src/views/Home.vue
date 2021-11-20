f0336d85406ad05ba7abb2048bb83e8d5c823999
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-20 20:23:43 -0300

    add inicial router

---
+++ atividades/projeto-spa/my-food/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <CategoryMenu />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CategoryMenu from '@/components/CategoryMenu.vue'
+export default {
+  name: 'Home',
+  components: {
+    CategoryMenu
+  }
+}
+</script>
