6dd2a87164d776d25475434c31d820e41f3cd5ee
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-21 12:49:33 -0300

    create file ItemList.vue

---
+++ atividades/projeto-spa/my-food/src/components/ItemList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="item-list">
+      hello
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ItemList',    
+    created() {
+    axios.get('http://localhost:3000/burguers').then(response => {
+      console.log(response);
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- atividades/projeto-spa/my-food/src/views/Home.vue
+++ atividades/projeto-spa/my-food/src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
     <CategoryMenu />
+    <ItemList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CategoryMenu from '@/components/CategoryMenu.vue'
-import axios from 'axios';
+import ItemList from '../components/ItemList.vue'
+
 export default {
   name: 'Home',
   components: {
-    CategoryMenu
-  }, 
-  created() {
-    axios.get('http://localhost:3000/burguers').then(response => {
-      console.log(response);
-    })
+    CategoryMenu,
+    ItemList
   }
 }
 </script>
+
+<style>
+
+.home {
+  display: flex;
+}
+
+</style>
