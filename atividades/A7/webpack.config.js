d273f24aac5117db5c4c6b64ace8d1121095d2e2
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-28 13:21:59 -0300

    install react structure

--- atividades/A7/webpack.config.js
+++ atividades/A7/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     /* entry: 'src/index.jsx' /*Indica o arquivo inical que vai começar a nossa aplicação*/
