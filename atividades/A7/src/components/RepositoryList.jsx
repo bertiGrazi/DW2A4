7e29c645e61278f89cb90ac4d64c25a892feabaf
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-28 19:57:05 -0300

    creat properties

--- atividades/A7/src/components/RepositoryList.jsx
+++ atividades/A7/src/components/RepositoryList.jsx
@@ -1,16 +1,19 @@
-const repositoryName = 'Atividade 1'
+import { RepositoryItem } from "./RepositoryItem";
 
+const repository = {
+    name: "Atividade",
+    description: "Descrição da Atividade",
+    link: "https://github.com/bertiGrazi/DW2A4"
+}
 
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar o repositório</a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
