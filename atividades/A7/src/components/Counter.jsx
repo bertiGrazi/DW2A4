95fd1ff37a701af98c0f75e1091cd29b47bc1cd6
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-28 20:29:14 -0300

    config webpack fast refresh

--- atividades/A7/src/components/Counter.jsx
+++ atividades/A7/src/components/Counter.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react' //guarda o estato de uma determinada variável
 
 
 export function Counter() {
+    //São imutabilidade = Seu valor não pode ser alterado
     const [counter, setCounter] = useState(0); //definindo o valor como zero e retorna um Array de dois elementos
 
     function increment() {
-        setCounter(counter + 1)
+        setCounter(counter + 1) //Criando uma nova variável em um espaço de memória
     }
     return(
         <div>
